45ebfb2b43b8c4c711060093e38cab9bca94ca44
studioplace <webstudioplace.com>
2016-08-10 17:09:24 -0400

    create server

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http'); //add the http module
+var myServer = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write("<b>Hello</b> World");
+  response.end();
+}); //create a server
+
+myServer.listen(3000);
